cf331b45e1b9b94133fe426e55bc8b61ff59669c
lecomtejac <lecomtejac@gmail.com>
2025-12-30 09:17:02 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Mémoire de la Marine</h1>
+      <p>Site collaboratif des lieux de mémoire maritime</p>
+    </main>
+  );
+}
