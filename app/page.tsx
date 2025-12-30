155bab6794d7e547cf12466833d800421b4b3400
lecomtejac <lecomtejac@gmail.com>
2025-12-30 16:58:16 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,8 @@
 
 import { useEffect, useState } from 'react';
 import Link from 'next/link';
-import { supabase } from '../../lib/supabaseClient';
+// Chemin corrigé : depuis app/page.tsx vers lib/supabaseClient.ts
+import { supabase } from '../lib/supabaseClient';
 
 interface Lieu {
   id: string;
