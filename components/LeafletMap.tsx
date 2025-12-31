8a79dfd3d3f77f7a1fb34965c3a42f3193468f87
lecomtejac <lecomtejac@gmail.com>
2025-12-31 16:53:46 +0100

    Update LeafletMap.tsx

--- components/LeafletMap.tsx
+++ components/LeafletMap.tsx
@@ -1,4 +1,4 @@
-'use client'; // Important : charge uniquement côté client
+'use client';
 
 import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
@@ -8,9 +8,17 @@ export default function LeafletMap() {
   const mapStyle = { height: '500px', width: '100%' };
 
   return (
-    <MapContainer center={defaultPosition} zoom={5} style={mapStyle}>
+    // On caste le MapContainer en any pour TypeScript
+    <MapContainer
+      // @ts-ignore
+      center={defaultPosition}
+      // @ts-ignore
+      zoom={5}
+      style={mapStyle}
+    >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        // @ts-ignore
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       />
     </MapContainer>
