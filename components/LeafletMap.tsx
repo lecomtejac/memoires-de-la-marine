3d3bdceb5273a30ac3be65ab0886a36cef53d6e5
lecomtejac <lecomtejac@gmail.com>
2025-12-31 16:52:04 +0100

    Update LeafletMap.tsx

--- components/LeafletMap.tsx
+++ components/LeafletMap.tsx
@@ -1,19 +1,10 @@
-'use client';
+'use client'; // Important : charge uniquement côté client
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
-
-// Correction icône par défaut
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
-  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
-  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
-});
 
 export default function LeafletMap() {
-  const defaultPosition: [number, number] = [48.8566, 2.3522];
+  const defaultPosition: [number, number] = [48.8566, 2.3522]; // Paris
   const mapStyle = { height: '500px', width: '100%' };
 
   return (
@@ -22,9 +13,6 @@ export default function LeafletMap() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       />
-      <Marker position={defaultPosition}>
-        <Popup>C'est un test ! 🌊</Popup>
-      </Marker>
     </MapContainer>
   );
 }
