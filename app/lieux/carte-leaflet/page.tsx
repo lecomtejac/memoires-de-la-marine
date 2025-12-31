dada79dc6fc89d64ba645749614fc8dd76a7796b
lecomtejac <lecomtejac@gmail.com>
2025-12-31 17:07:40 +0100

    Update page.tsx

--- app/lieux/carte-leaflet/page.tsx
+++ app/lieux/carte-leaflet/page.tsx
@@ -1,14 +1,12 @@
-'use client';
-
 import LeafletMap from '@/components/LeafletMap';
 
-export default function Page() {
+export default function CarteLeafletPage() {
   const defaultPosition: [number, number] = [48.8566, 2.3522]; // Paris
 
   return (
     <div style={{ padding: '2rem' }}>
-      <h1>Test carte Leaflet avec marker</h1>
-      <LeafletMap position={defaultPosition} zoom={5} />
+      <h1>Test carte Leaflet avec Marker</h1>
+      <LeafletMap position={defaultPosition} />
     </div>
   );
 }
