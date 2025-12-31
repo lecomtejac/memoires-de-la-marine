28447bda9d1a8672a3525537bca8535608812f1b
lecomtejac <lecomtejac@gmail.com>
2025-12-31 16:45:41 +0100

    Update page.tsx

--- app/lieux/carte-leaflet/page.tsx
+++ app/lieux/carte-leaflet/page.tsx
@@ -1,11 +1,13 @@
-'use client'; // important : rend ce composant client-side
+'use client';
 
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer as LeafletMapContainer, TileLayer } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 export default function Page() {
   const defaultPosition: [number, number] = [48.8566, 2.3522]; // Paris
   const mapStyle = { height: '500px', width: '100%' };
+
+  const MapContainer: any = LeafletMapContainer; // <-- CAST any pour TS
 
   return (
     <div style={{ padding: '2rem' }}>
