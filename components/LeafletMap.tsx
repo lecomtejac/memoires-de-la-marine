b3fab2102986de89d9181feadbd9a899291cbb32
lecomtejac <lecomtejac@gmail.com>
2025-12-31 17:07:17 +0100

    Update LeafletMap.tsx

--- components/LeafletMap.tsx
+++ components/LeafletMap.tsx
@@ -3,28 +3,27 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
-import { LatLngExpression } from 'leaflet';
+import { useEffect } from 'react';
+
+// Corriger l'icône du marker par défaut (Leaflet et Next.js)
+delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+});
 
 interface LeafletMapProps {
-  position: LatLngExpression;
+  position: [number, number];
   zoom?: number;
 }
 
 export default function LeafletMap({ position, zoom = 5 }: LeafletMapProps) {
   const mapStyle = { height: '500px', width: '100%' };
 
-  // Correct marker icon (sinon il n'apparaît pas)
-  const defaultIcon = L.icon({
-    iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
-    iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
-    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
-    iconSize: [25, 41],
-    iconAnchor: [12, 41],
-  });
-  L.Marker.prototype.options.icon = defaultIcon;
-
   return (
-    <MapContainer center={position} zoom={zoom} style={mapStyle} key={position.toString()}>
+    <MapContainer center={position} zoom={zoom} style={mapStyle}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
