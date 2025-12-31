cc8502708ac5827fad55e1d528816baaadc6c4bd
lecomtejac <lecomtejac@gmail.com>
2025-12-31 17:01:11 +0100

    Update LeafletMap.tsx

--- components/LeafletMap.tsx
+++ components/LeafletMap.tsx
@@ -3,37 +3,34 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
+import { LatLngExpression } from 'leaflet';
 
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl:
-    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
-  iconUrl:
-    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
-  shadowUrl:
-    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
-});
-
-// On définit un type pour les props
 interface LeafletMapProps {
-  position: [number, number]; // latitude et longitude
+  position: LatLngExpression;
   zoom?: number;
 }
 
 export default function LeafletMap({ position, zoom = 5 }: LeafletMapProps) {
   const mapStyle = { height: '500px', width: '100%' };
 
+  // Correct marker icon (sinon il n'apparaît pas)
+  const defaultIcon = L.icon({
+    iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+    iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+    shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+  });
+  L.Marker.prototype.options.icon = defaultIcon;
+
   return (
-    // @ts-ignore pour contourner les erreurs TypeScript
-    <MapContainer center={position} zoom={zoom} style={mapStyle}>
+    <MapContainer center={position} zoom={zoom} style={mapStyle} key={position.toString()}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        // @ts-ignore
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
       />
       <Marker position={position}>
-        <Popup>Marker Test</Popup>
+        <Popup>Marker test</Popup>
       </Marker>
     </MapContainer>
   );
