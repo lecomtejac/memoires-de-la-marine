b75794a57538fdc5fdc444c959eb5d723a4726b0
lecomtejac <lecomtejac@gmail.com>
2026-01-01 19:39:37 +0100

    Update LeafletMapSupabase.tsx

--- components/LeafletMapSupabase.tsx
+++ components/LeafletMapSupabase.tsx
@@ -91,7 +91,21 @@ export default function LeafletMapSupabase() {
         ))}
 
         {userPosition && (
-          <Marker position={userPosition} icon={new L.Icon.Default()}>
+          <Marker
+            position={userPosition}
+            icon={L.icon({
+              iconUrl:
+                'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon.png',
+              iconRetinaUrl:
+                'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-icon-2x.png',
+              shadowUrl:
+                'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/images/marker-shadow.png',
+              iconSize: [25, 41],
+              iconAnchor: [12, 41],
+              popupAnchor: [1, -34],
+              shadowSize: [41, 41],
+            })}
+          >
             <Popup>Vous êtes ici</Popup>
           </Marker>
         )}
