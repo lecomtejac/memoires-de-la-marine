ba4b8553554664209c127e8f579c3bbf4ef3b38e
lecomtejac <lecomtejac@gmail.com>
2026-01-01 10:36:15 +0100

    Update page.tsx

--- app/lieux/test-carte-leaflet/page.tsx
+++ app/lieux/test-carte-leaflet/page.tsx
@@ -2,13 +2,13 @@ import dynamic from 'next/dynamic';
 
 const LeafletMapSupabase = dynamic(
   () => import('../../../components/LeafletMapSupabase'),
-  { ssr: false }
+  { ssr: false } // Important pour que Leaflet et useEffect fonctionnent côté client
 );
 
 export default function Page() {
   return (
     <div style={{ padding: '2rem' }}>
-      <h1>Test Leaflet + Supabase</h1>
+      <h1>Test Carte Leaflet + Supabase</h1>
       <LeafletMapSupabase />
     </div>
   );
