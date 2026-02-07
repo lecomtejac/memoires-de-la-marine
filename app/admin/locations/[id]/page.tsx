4324d44c894af986b4804d920287036d4bc1d27d
lecomtejac <lecomtejac@gmail.com>
2026-02-07 16:12:31 +0100

    Update page.tsx

--- app/admin/locations/[id]/page.tsx
+++ app/admin/locations/[id]/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { supabase } from '../../../../lib/supabaseClient';
+import Link from 'next/link';
 
 // Typage
 interface Location {
